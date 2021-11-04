a645a4d55b21f10a148cea9a9738187d70aa1faf
dobothor <70290147+dobothor@users.noreply.github.com>
2021-11-04 15:55:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ import re
 from io import BytesIO, StringIO
 import numpy as np
 from PIL import Image
+from itertools import *
+from igraph import *
+#https://stackoverflow.com/questions/41957490/send-canvas-image-data-uint8clampedarray-to-flask-server-via-ajax
+#https://www.abeautifulsite.net/posts/postjson-for-jquery/
+
+def to_i(y, x):
+    return y * im.shape[1] + x
+
+def to_coord(index):
+    return int((index-index % im.shape[1]) / im.shape[1]), index % im.shape[1]
+
+r = .1
 
 app = Flask(__name__)
 
@@ -29,28 +41,46 @@ def paintapp():
 #@csrf.exempt
 @app.route('/savep', methods=['GET','POST'])
 def savep():
-    #conn = psycopg2.connect(database="paintmyown", user="nidhin")
-    #cur = conn.cursor()
-    #cur.execute("SELECT id, name, data, canvas_image from files")
-    #files = cur.fetchall()
-    #conn.close()
-    
-    #g = Github(login_or_token="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") 
-    #repo = g.get_user("dobothor").get_repo("optimal_road")
-    #content = "Hello Web!"
-    #repo.create_file("images/text3.txt","commiting files", content)
-    print("Hello!")
-    #test_text = request.values['test']
-    #print(test_text)
     image_b64 = request.values['imageBase64']
-    print(image_b64)
-    print(type(image_b64))
-    image_b64 = image_b64[22:]
-    print(image_b64)
-    #image_data = re.sub('^data:image/.+;base64,', '', image_b64).decode('base64')
+    #print(image_b64)
+    #print(type(image_b64))
+    image_b64 = image_b64[22:]  #image comes encoded with beginning 'data:image/png;base64,'
+    #print(image_b64)
     image_PIL = Image.open(BytesIO(base64.b64decode(image_b64)))
     image_np = np.array(image_PIL)
+    #analyze image
+    #lim = [list(j[0:3]) for i in im for j in i]
+    #width = len(im[0])
+    #height = len(im)
+    #edg=[]
+    #dire = list(product([0,1,-1],[0,1,-1]))
+    #for i in range(len(lim)):
+    #    for y_dif, x_dif in dire:
+    #        coord = to_coord(i)
+    #        if coord[0] < 1 or coord[1] < 1 or coord[0] == height-1 or coord[1] == width-1 :
+    #            continue
+    #        #g.add_edge(i,to_i(List[i][0]+x_dif,List[i][1]+y_dif),weight=1)
+    #        if abs(y_dif+x_dif) == 1:
+    #            diag = 1
+    #        else:
+    #            diag = (1+1)**.5
+    #        if lim[i]==[255,255,255]:
+    #            edg.append( (i, to_i(coord[0]+x_dif,coord[1]+y_dif), 1*diag) )
+    #        else:
+    #            edg.append( (i, to_i(coord[0]+x_dif,coord[1]+y_dif), r*diag) )
+
+    #G = Graph.TupleList(edg, weights=True)
+    #weight = G.es['weight']     #check if shortest_paths produces smaller pkl
+    #dist = G.shortest_paths_dijkstra(weights=weight)
+
+    #dist_list = [y for x in dist for y in x]
+    #roads = [0 if i==[255,255,255] else 1 for i in lim]
+    #calculate score
+    #upload image with name to imbgg
+    #done
     print("Image received:",(image_np.shape))
+    
+    
     return "nothing"  #render_template("paint.html") #render_template("save.html", files = files )
     
 @app.route('/search', methods=['GET', 'POST'])
