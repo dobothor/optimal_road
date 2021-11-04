d43ef4e8bd155c3c76ea32daf31ab12c0a07ea85
dobothor <70290147+dobothor@users.noreply.github.com>
2021-11-04 09:41:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def savep():
     image_b64 = request.values['imageBase64']
     #image_data = re.sub(
     image_PIL = Image.open(cStringIO.StringIO(image_b64))
-    print("Image received: {}'.format(image_np.shape
+    print("Image received: {}'.format(image_np.shape))
     return "nothing" #render_template("save.html", files = files )
     
 @app.route('/search', methods=['GET', 'POST'])
