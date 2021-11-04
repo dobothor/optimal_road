fc03ee1ce6a5a281f377e7509183ca1ef9798c2f
dobothor <70290147+dobothor@users.noreply.github.com>
2021-11-04 10:34:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def paintapp():
         return redirect(url_for('save'))        
         
         
-@app.route('/savep', methods=['GET', 'POST'])
+@app.route('/savep', methods=['POST'])
 def savep():
     #conn = psycopg2.connect(database="paintmyown", user="nidhin")
     #cur = conn.cursor()
