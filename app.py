946ab9e3bcdb5088e4a059942adb7634976ade35
dobothor <70290147+dobothor@users.noreply.github.com>
2021-11-15 11:09:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def savep():
     #print("Image received:",(image_np.shape))
     
     
-    return 1 #render_template("save.html") #"nothing"  #render_template("paint.html") #render_template("save.html", files = files )
+    return "nothing" #render_template("save.html") #  #render_template("paint.html") #render_template("save.html", files = files )
     
 @app.route('/search', methods=['GET', 'POST'])
 def search():
